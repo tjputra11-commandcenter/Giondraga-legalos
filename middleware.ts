8ccbc30df2b8b8d5c48c94d6599b12b62bb86e81
tjputra11-commandcenter <tj.putra11@gmail.com>
2026-05-15 13:00:16 +0700

    Fix middleware cookiesToSet type

--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             request.cookies.set(name, value)
             response.cookies.set(name, value, options)
